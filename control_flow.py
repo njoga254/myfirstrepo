5fcb6fd885423c37d3ed95a66a32b3d0c0e0f926
njoga lawrence <njogalawrence@gmail.com>
2024-09-17 22:57:59 +0300

    Modified control_flow.py

--- control_flow.py
+++ control_flow.py
@@ -9,16 +9,3 @@ else:
 
 final_price = purchase_amount * (1 - discount)
 print("Final price after discount: $" (final_price))
-
-
-score = int(input("Enter your score: "))
-if score >= 90:
-    grade = "A"
-elif score >= 80:
-    grade = "B"
-elif score = >= 70:
-    grade = "C"
-else:
-    grade = "F"
-
-print("Your grade is: ", grade)
