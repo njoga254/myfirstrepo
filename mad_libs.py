a26dfd3348adf9ccfffe839069a2ac16f163f247
njoga lawrence <njogalawrence@gmail.com>
2024-09-19 00:53:44 +0300

    Modified mad libs file

--- mad_libs.py
+++ mad_libs.py
@@ -16,10 +16,11 @@ if adjective_2 and adjective_3 and adjective_4 == descriptive_noun:
     adjective_comment = "pass"
 else:
     adjective_comment = "fail"
-
+#storyline with the f-string.
 story = f"""
-         On a beautiful {adjective_1} day,
-         I went to the zoo. I saw a funny {adjective_2}
-         monkey swinging from the trees. Then, spotted a majestic {adjective_3}
-         lion lounging in the sun. What a wild and {adjective_4} experience!"""
+         On a beautiful {adjective_1} day, I went to the zoo. 
+         I saw a funny {adjective_2} monkey swinging from the trees.
+         Then, spotted a majestic {adjective_3} lion lounging in the sun. What a wild and {adjective_4} experience!
+         """ 
+         
 print(story)
