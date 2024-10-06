b259be7ed49946f8dec82f41fdb3bf4536f1c772
njoga lawrence <njogalawrence@gmail.com>
2024-10-06 10:31:37 +0300

    Add zerodivisionerror file

---
+++ zerodivisionerror.py
@@ -0,0 +1,9 @@
+num1 = int(input("Input the first integer: "))
+num2 = int(input("Input the second integer: "))
+if num2 == 0:
+    print(f"Error! You can not divide by zero.")
+else:
+    division = num1 / num2
+    print(f"The result of {num1} divided by {num2} is: {division}")
+
+
